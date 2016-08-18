7ed38d75d30b8b024b8e046ef3129b183277b194
Travis Gorman <thefarbetween@me.com>
2016-08-18 11:05:08 -0500

    quick commit in case i break everything while crackilackin

--- app/scripts/components/Welcome.js
+++ app/scripts/components/Welcome.js
@@ -1,20 +1,50 @@
 import React from 'react';
-
 import Header from './Header';
 import UserModel from '../Models/UserModel';
 import UserCollection from '../Collections/UserCollection';
 import store from '../store';
 
 export default React.createClass({
-  componentDidMount: function () {
-    console.log( 'store.session.attributes...', store.sessionModel.attributes );
+  getInitialState: function () {
+    return {
 
+    }
+  },
+  componentDidMount: function () {
+    // search collection
+  },
+  submitHandler: function (e) {
+    e.preventDefault();
+    console.log(e);
   },
   render: function() {
     return (
       <div>
       <Header/>
         Welcome!
+      <form
+        onSubmit={this.submitHandler} >
+
+        <input
+          type="text"
+          placeholder="location"
+          ref="searchLocation" />
+
+        <input
+          type="text"
+          placeholder="skills"
+          ref="searchSkills" />
+
+        <input
+          type="submit"
+          value="search" />
+
+      </form>
+      <main
+        className="searchResults" >
+        
+
+      </main>
       </div>
     )
   }
